f60b6e917c8d5778dbca593ec9e6f8e07d932728
Frank S. Thomas <fthomas@physik.uni-wuerzburg.de>
2010-10-03 09:29:33 +0200

    Simplified block input.

--- doc/examples/06_extended_output.cpp
+++ doc/examples/06_extended_output.cpp
@@ -19,13 +19,13 @@ int main(int, char* [])
   Coll out1;
   string block = "SMINPUTS";
   out1[block][""] << "BLOCK" << block;
-  out1[block][""] <<  "1"    << alpha_em << "# alpha_em";
-  out1[block][""] <<  "2"    << G_mu     << "# G_mu";
-  out1[block][""] <<  "3"    << alpha_s  << "# alpha_s";
-  out1[block][""] <<  "4"    << m_Z      << "# m_Z";
-  out1[block][""] <<  "5"    << m_b      << "# m_b";
-  out1[block][""] <<  "6"    << m_t      << "# m_t";
-  out1[block][""] <<  "7"    << m_tau    << "# m_tau";
+  out1[block][""] <<   1     << alpha_em << "# alpha_em";
+  out1[block][""] <<   2     << G_mu     << "# G_mu";
+  out1[block][""] <<   3     << alpha_s  << "# alpha_s";
+  out1[block][""] <<   4     << m_Z      << "# m_Z";
+  out1[block][""] <<   5     << m_b      << "# m_b";
+  out1[block][""] <<   6     << m_t      << "# m_t";
+  out1[block][""] <<   7     << m_tau    << "# m_tau";
 
   stringstream ss;
   ss.precision(8);
