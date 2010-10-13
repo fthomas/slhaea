c9aa9fb498ec6eb37949899ad2176f00a6606034
Frank S. Thomas <fthomas@physik.uni-wuerzburg.de>
2010-10-13 22:51:17 +0200

    Played with the Ruby implementation.

--- ruby/slhaea.rb
+++ ruby/slhaea.rb
@@ -1,22 +1,44 @@
 class Line
+  include Enumerable
+
   def initialize(line)
     @cont = Array.new
-    #from_s(line) if line.is_a?(String)
+    from_s(line)
   end
 
-  #def from_s
-  #end
+  def from_s(line)
+    clear
+    line = line.to_s
+    stripped_line = line[/^.*$/].rstrip
+    return self if stripped_line.empty?
+  end
 
-  #def to_s
-  #end
+  def to_s
+  end
 
-  def to_a; @cont; end
+  def to_a
+    @cont
+  end
 
-  def [](index); @cont[index]; end
+  def [](index)
+    @cont[index]
+  end
 
-  #def []=(index, obj); 
+  def each(block)
+    @cont.each(block)
+  end
 
-  def empty?; @cont.empty?; end
+  def size
+    @cont.size
+  end
 
-  def size; @cont.size; end
+  def empty?
+    @cont.empty?
+  end
+
+  def clear
+    @cont.clear
+  end
 end
+
+# vim: sw=2 tw=78
