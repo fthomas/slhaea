00d50475b4f58361b745758e513db52a1342a30a
Frank S. Thomas <fthomas@physik.uni-wuerzburg.de>
2010-10-24 01:54:15 +0200

    Adapted example to the new formatting.

--- doc/examples/06_extended_output.cpp
+++ doc/examples/06_extended_output.cpp
@@ -19,25 +19,25 @@ int main(int, char* [])
   Coll out1;
   string block = "SMINPUTS";
   out1[block][""] << "BLOCK" << block;
-  out1[block][""] <<   1     << alpha_em << "# alpha_em";
-  out1[block][""] <<   2     << G_mu     << "# G_mu";
-  out1[block][""] <<   3     << alpha_s  << "# alpha_s";
-  out1[block][""] <<   4     << m_Z      << "# m_Z";
-  out1[block][""] <<   5     << m_b      << "# m_b";
-  out1[block][""] <<   6     << m_t      << "# m_t";
-  out1[block][""] <<   7     << m_tau    << "# m_tau";
+  out1[block][""] <<      1  << alpha_em << "# alpha_em";
+  out1[block][""] <<      2  << G_mu     << "# G_mu";
+  out1[block][""] <<      3  << alpha_s  << "# alpha_s";
+  out1[block][""] <<      4  << m_Z      << "# m_Z";
+  out1[block][""] <<      5  << m_b      << "# m_b";
+  out1[block][""] <<      6  << m_t      << "# m_t";
+  out1[block][""] <<      7  << m_tau    << "# m_tau";
 
   stringstream ss;
   ss.precision(8);
   ss.setf(ios_base::scientific);
   ss << "BLOCK SMINPUTS\n"
-     << " 1  " << alpha_em << "  # alpha_em\n"
-     << " 2  " << G_mu     << "  # G_mu\n"
-     << " 3  " << alpha_s  << "  # alpha_s\n"
-     << " 4  " << m_Z      << "  # m_Z\n"
-     << " 5  " << m_b      << "  # m_b\n"
-     << " 6  " << m_t      << "  # m_t\n"
-     << " 7  " << m_tau    << "  # m_tau\n";
+     << "    1   " << alpha_em << "  # alpha_em\n"
+     << "    2   " << G_mu     << "  # G_mu\n"
+     << "    3   " << alpha_s  << "  # alpha_s\n"
+     << "    4   " << m_Z      << "  # m_Z\n"
+     << "    5   " << m_b      << "  # m_b\n"
+     << "    6   " << m_t      << "  # m_t\n"
+     << "    7   " << m_tau    << "  # m_tau\n";
   Coll out2(ss);
 
   cout << out1;
